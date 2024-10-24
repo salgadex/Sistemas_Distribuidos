ccc46dc820defb0e1573116420486b043c9ede95
Bernardo <salgadobernardo123@gmail.com>
2024-10-24 12:32:32 +0100

    Exercise 1 done by the teacher.

--- src/CinemaImpl.java
+++ src/CinemaImpl.java
@@ -7,21 +7,21 @@ public class CinemaImpl implements Cinema{
     private final Lock lock = new ReentrantLock();
     private final Condition filmeDisponivel = lock.newCondition();
     private String filmeAtual = null;
-    private Set<String> cinefilosNaSala = new HashSet<>();
+    private int naSala = 0;
 
     public void assiste(String filme) {
         this.lock.lock();
         try{
-            while(filmeAtual != null && !filmeAtual.equals(filme)){
+            while(this.filmeAtual != null && !this.filmeAtual.equals(filme) && naSala > 0){
                 filmeDisponivel.await();
             }
-            filmeAtual = filme;
-            cinefilosNaSala.add(Thread.currentThread().getName());
+            this.filmeAtual = filme;
+            naSala++;
+
         } catch (InterruptedException e) {
-            Thread.currentThread().interrupt();
-            throw new RuntimeException("Thread interrompida", e);
-        }
-        finally{
+            throw new RuntimeException(e);
+
+        } finally{
             this.lock.unlock();
         }
 
@@ -30,24 +30,28 @@ public class CinemaImpl implements Cinema{
     }
 
     public void abandona(String filme) {
-       this.lock.lock();
-       try {
-           cinefilosNaSala.remove(Thread.currentThread().getName());
-           if(cinefilosNaSala.isEmpty()){
-               filmeAtual = null;
-               filmeDisponivel.signalAll();
-           }
-       }
+        this.lock.lock();
+        try {
+            naSala--;
+            if(naSala == 0){
+                filmeDisponivel.signalAll();
+            }
+        }
 
-       finally {
-           this.lock.unlock();
-       }
+        finally {
+            this.lock.unlock();
+        }
     }
 
     public String filmeEmExibicao() {
         this.lock.lock();
         try {
-             return filmeAtual != null ? filmeAtual : "NENHUM";
+            if(filmeAtual == null) {
+                return "Nenhum";
+            }
+            else {
+                return this.filmeAtual;
+            }
         }
 
         finally {
